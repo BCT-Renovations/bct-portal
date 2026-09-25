521c7f96d7d32323cec787d2e0af3502065a39f5
BCT-Renovations <myproject@bctrenovations.com>
2026-09-24 22:44:50 -0400

    Add BCT portal service worker

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME='bct-portal-shell-v1';
+const APP_SHELL=['/','/index.html','/manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('/index.html'))));
+});
